b621381e5fda4c8acf0901fb79f9ca0654ffe3e1
Jim Hurst <james.hurst@comparethemarket.com>
2017-05-11 15:09:09 +0100

    Added a initial C# and NUnit framework

---
+++ csharp/DevTest.CSharp/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTest.CSharp
+{
+    public class Class1
+    {
+        public string Hello()
+        {
+            return "hi";
+        }
+    }
+}
